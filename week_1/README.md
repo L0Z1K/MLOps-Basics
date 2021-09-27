ed194dce614707b126cb5ba6f9d487f32a821f7a
L0Z1K <qortmddbs731@naver.com>
2021-09-27 06:11:18 +0000

    feat: copy week_0 script

---
+++ week_1/README.md
@@ -0,0 +1,11 @@
+## Week 1: Model monitoring - Weights and Biases
+
+### Train & Infer
+
+```bash
+python train.py
+```
+
+```bash
+python infer.py
+```
---
+++ week_1/infer.py
@@ -0,0 +1,33 @@
+import torch
+from model import CoLAModel
+from data import DataModule
+
+
+class CoLAPredictor:
+    def __init__(self, model_path):
+        self.model_path = model_path
+        self.model = CoLAModel.load_from_checkpoint(model_path)
+        self.model.eval()
+        self.model.freeze()
+        self.processor = DataModule()
+        self.softmax = torch.nn.Softmax(dim=0)
+        self.labels = ["unacceptable", "acceptable"]
+    
+    def predict(self, text):
+        inference_sample = {"sentence": text}
+        processed = self.processor.tokenize_data(inference_sample)
+        logits = self.model(
+            torch.tensor([processed["input_ids"]]),
+            torch.tensor([processed["attention_mask"]]),
+        )
+        scores = self.softmax(logits[0]).tolist()
+        predictions = []
+        for score, label in zip(scores, self.labels):
+            predictions.append({"label": label, "score": score})
+        return predictions
+
+
+if __name__ == "__main__":
+    sentence = input("Input the sentence for inference: ")
+    predictor = CoLAPredictor("./models/epoch=3-step=1071.ckpt")
+    print(predictor.predict(sentence))
