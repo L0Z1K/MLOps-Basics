728e3c8a0a05367f8939df8fe03ed1ae6e624a8e
L0Z1K <qortmddbs731@naver.com>
2021-12-05 12:02:01 +0000

    feat: Copy from week2

---
+++ week_3/train.py
@@ -0,0 +1,58 @@
+import torch
+import hydra
+import logging
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import WandbLogger
+from omegaconf.omegaconf import OmegaConf
+
+from data import DataModule
+from model import CoLAModel
+
+
+logger = logging.getLogger(__name__)
+
+
+@hydra.main(config_path='./configs', config_name='config')
+def main(cfg):
+    logger.info(OmegaConf.to_yaml(cfg, resolve=True))
+    logger.info(f"Using the model: {cfg.model.name}")
+    logger.info(f"Using the tokenizer: {cfg.model.tokenizer}")
+    cola_data = DataModule(
+        cfg.model.tokenizer, cfg.processing.batch_size
+    )
+    cola_model = CoLAModel(
+        cfg.model.name
+    )
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="./models",
+        filename="best.ckpt",
+        monitor="valid/loss",
+        mode="min"
+    )
+    early_stopping_callback = EarlyStopping(
+        monitor="valid/loss", patience=3, verbose=True, mode="min"
+    )
+
+    wandb_logger = WandbLogger(project="MLOps Basics",
+                               entity="sybaek",
+                               name="bert")
+
+    trainer = pl.Trainer(
+        default_root_dir="logs",
+        gpus=(1 if torch.cuda.is_available() else 0),
+        max_epochs=cfg.training.max_epochs,
+        fast_dev_run=False,
+        logger=wandb_logger,
+        callbacks=[checkpoint_callback,
+                   early_stopping_callback],
+        log_every_n_steps=cfg.training.log_every_n_steps,
+    )
+
+    trainer.fit(cola_model, cola_data)
+
+
+if __name__ == "__main__":
+    main()
